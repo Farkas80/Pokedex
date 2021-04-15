af0d9216c872ae840d928d0804b608fdc7586cbc
Farkas80 <csaba.farkas80@gmail.com>
2021-04-15 14:59:28 +0200

    Build it more efficiently

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const fetchPokemon = () => {
+/*const fetchPokemon = () => {
+    This solution is fast enough but it has its disadvantege. Asynch used in a for loop is a bad practice
+    because one for loop has to be finished before js can execute for example the next foor loop.
     for (let i = 1; i <=150; i++) { 
-    const url = `https://pokeapi.co/api/v2/pokemon/1`;
+    const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     fetch(url)
         .then((res) => {
             return res.json();
@@ -17,6 +19,31 @@ const fetchPokemon = () => {
             console.log(pokemon);
         });
     };
+};
+
+fetchPokemon();
+*/
+// It is better to queue things up and run paralel not sequentially. 
+
+const fetchPokemon = () => {
+
+    //create an empty array of promises
+    const promises = [];
+    for (let i=1; i <= 150; i++) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+        /*pushing a new promise in the array*/
+        promises.push(fetch(url)).then((res) => res.json());
+    };
+
+    Promise.all(promises).then((results) => {
+        const pokemon = results.map((data) => ({
+            name: data.name,
+                id: data.id,
+                image: data.sprites['front_default'],
+                type: data.types.map((type) => type.type.name).join(', ')
+        }));
+    });
+
 };
 
 fetchPokemon();
