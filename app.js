d0d0c85ceddc378e89fe14a8ad310146846b7654
Farkas80 <csaba.farkas80@gmail.com>
2021-04-15 11:36:24 +0200

    Update js to log out all the 150 pokemons

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Mindent bele!');
+const fetchPokemon = () => {
+    for (let i = 1; i <=150; i++) { 
+    const url = `https://pokeapi.co/api/v2/pokemon/1`;
+    fetch(url)
+        .then((res) => {
+            return res.json();
+        })
+        .then((data) => {
+            console.log(data);
+
+            pokemon = {
+                name: data.name,
+                id: data.id,
+                image: data.sprites['front_default'],
+                type: data.types.map((type) => type.type.name).join(', ')
+            };
+            console.log(pokemon);
+        });
+    };
+};
+
+fetchPokemon();
